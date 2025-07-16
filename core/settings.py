46d451b8ac47a9b2f55c2c1bf6a98a298d9ed374
Pavan Kalyan S <pk20180708248@gmail.com>
2025-07-16 22:54:40 +0530

    chore: add Procfile for Gunicorn deployment and update requirements.txt to include Whitenoise

--- core/settings.py
+++ core/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
 
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -124,7 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
