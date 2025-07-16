6089f19607db535bfb583dab2b4436901e5a2017
Pavan Kalyan S <pk20180708248@gmail.com>
2025-07-16 23:44:35 +0530

    chore: simplify WSGI configuration by removing comments and unnecessary imports

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import os
+import sys
+print(">>> WSGI starting...", file=sys.stderr)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
+
+print(">>> WSGI app loaded", file=sys.stderr)
